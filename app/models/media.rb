cb424a887c64b12dbfdf026c800fc9d950b81f17
martin-sean <29813268+martin-sean@users.noreply.github.com>
2019-09-15 23:00:07 +1000

    Building media list

--- app/models/media.rb
+++ app/models/media.rb
@@ -1,6 +1,6 @@
 class Media < ApplicationRecord
   has_many :ratings, dependent: :destroy
-  has_many :users, through: ratings
+  has_many :users, through: :ratings
   has_many :genres
   has_many :actors
   has_one :director
