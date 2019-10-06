95cf6f7580a9e3ccdd8819023a2494e04eb47572
martin-sean <29813268+martin-sean@users.noreply.github.com>
2019-10-07 00:40:50 +1100

    Added MDB API key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionHelper
+
+  MDB_API_KEY = Rails.application.credentials[:mdb][:api]
 end
