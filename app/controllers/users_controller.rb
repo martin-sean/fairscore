a02d83cd2c3ab682c865e192b386fc07312d3873
martin-sean <29813268+martin-sean@users.noreply.github.com>
2019-10-12 20:02:56 +1100

    Fixed user scored ratings graph

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_graph_data, only: :show
 
   # GET /users
   # GET /users.json
@@ -61,6 +62,15 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+
+    # Generate scored ratings graph data
+    def set_graph_data
+      counts = @user.ratings.group(:score).count
+      @graph_data = Hash.new(0)
+      (0..10).each {|i| @graph_data[i] = 0 }
+      counts.each {|k, v| @graph_data[k] = v }
+      @graph_data.delete nil
     end
 
 end
