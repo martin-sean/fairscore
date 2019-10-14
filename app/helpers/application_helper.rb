4e6bc814ebc3e1af9481d153077d3aad4548dbf4
martin-sean <29813268+martin-sean@users.noreply.github.com>
2019-10-14 23:31:31 +1100

    Desparate testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,20 @@ module ApplicationHelper
     cache_key = "media-zscore-#{media_id}"
     score = Rails.cache.read(cache_key)
     # Check if score cache needs updating
-    if score.blank? || time_elapsed?(Time.now, score[:last_update], 5.minutes)
+    if score.blank?
+      @ratings = Rating.where(media_id: media_id)
+      zscore_sum = 0
+      scored_count = 0
+      # Sum the zscores and count the scored ratings
+      @ratings.each do |rating|
+        if rating.score.present?
+          zscore_sum += rating.zscore
+          scored_count += 1
+        end
+      end
+      score = zscore_sum.fdiv(scored_count)
+      Rails.cache.write(cache_key, { value: score, last_update: Time.current }) unless score.nan?
+    elsif time_elapsed?(Time.now, score[:last_update], 5.minutes)
       MediaScoreUpdateJob.perform_later(media_id, cache_key)
     end
     # Return if score exists
